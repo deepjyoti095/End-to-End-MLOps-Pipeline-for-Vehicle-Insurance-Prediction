fe7cca945615edf5d51900902ac19c0c1baefb9c
Deepjyoti <deepjyoti692@gmail.com>
2026-07-21 21:42:25 +0530

    Project structure ready, venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='src',
+    version='0.0.1',
+    author='Deepjyoti',
+    author_email="deepjyoti095@gmail.com",
+    pacakages=find_packages()
+)
